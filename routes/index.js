1f12294b0b6b8dd38e42fd267737bb1344ededab
kasperpagh <kasperpagh@gmail.com>
2019-03-19 14:51:25 +0100

    first stuff

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res, next)
 
 router.get('/test', function(req,res,next)
 {
-    res.end("hej fra Node")
+    res.end("hej fra Node og TC")
 });
 
 module.exports = router;
